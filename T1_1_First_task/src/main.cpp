7a9cdb800691a3a71ee9ba557d4a5669c2849d4c
Ji Zhongyuan <jiz1@vdiubuntu100.org.aalto.fi>
2025-01-28 16:57:38 +0200

    first

--- T1_1_First_task/src/main.cpp
+++ T1_1_First_task/src/main.cpp
@@ -4,5 +4,15 @@
 int main(void)
 {
     /* Copy the code after this comment. */
+
+
+ int a;
+    std::cout << "Hello there!" << std::endl;
+
+    a = 12;
+    std::cout << "a=" << a << std::endl;
+
+    a = 13;
+    std::cout << "a=" << a << std::endl;
         return 0;
 }
